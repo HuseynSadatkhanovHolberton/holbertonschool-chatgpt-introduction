7eb4895b5a927f2a5f66970e445b1b653b4221eb
SadatkhanovHuseyn <hsadatkhanov13770@adda.edu.az>
2026-04-03 18:37:37 +0400

    task 0 factorial

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 import sys
 
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1   # FIX: decrement n
+        n -= 1
     return result
 
 f = factorial(int(sys.argv[1]))
